6006a352556a81839cdf6f5b8d06da3af39d5316
markoowik <serik.alisher100605@gmail.com>
2025-12-30 11:39:14 +0500

    fix: правил

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -9,12 +11,9 @@ import authRoutes from "./src/routes/auth.js";
 import courseRoutes from "./src/routes/courseRoutes.js";
 import applicationRoutes from "./src/routes/applicationRoutes.js";
 
-dotenv.config();
 
 const app = express();
-app.use(cors({
-    origin: "https://avto-school.netlify.app",
-}));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -22,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI)
      .catch(err => console.log(err));
 
 app.use("/api/auth", authRoutes);
-app.use("/courses", courseRoutes);
+app.use("/api/courses", courseRoutes);
 app.use("/api/applications", applicationRoutes);
 
 
