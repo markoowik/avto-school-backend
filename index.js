ac295acb012b9c7f9cb68256eccfa479d214b316
markoowik <serik.alisher100605@gmail.com>
2026-01-03 13:42:32 +0500

    fix5

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
 
@@ -25,7 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/news", newsRoutes);
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 
 
