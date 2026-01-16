e9c77bd245e052a374237751408037f984f1b929
markoowik <serik.alisher100605@gmail.com>
2026-01-16 21:22:27 +0500

    fix20

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,28 +1,22 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
 
-export default async function authMiddleware(req, res, next) {
+export const authMiddleware = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
-
         if (!authHeader) {
-            return res.status(401).json({ message: "Нет авторизации" });
+            return res.status(401).json({ message: "Нет токена" });
         }
 
         const token = authHeader.split(" ")[1];
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await User.findById(decoded.id).select("-password");
+        req.user = {
+            id: decoded.id,   // 🔴 ВАЖНО
+        };
 
-        if (!user) {
-            return res.status(404).json({ message: "Пользователь не найден" });
-        }
-
-        req.user = user;
-        req.admin = decoded;
         next();
     } catch (e) {
-        return res.status(401).json({ message: "Неверный токен" });
+        return res.status(401).json({ message: "Не авторизован" });
     }
-}
+};
