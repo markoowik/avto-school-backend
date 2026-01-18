551bfa85df08803e029bc29b73b9abf9398dbab6
markoowik <serik.alisher100605@gmail.com>
2026-01-19 00:10:01 +0500

    fix35

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,23 +1,27 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const authMiddleware = (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            return res.status(401).json({ message: "Нет токена" });
+export const authMiddleware = () => {
+    return async (req, res, next) => {
+        try {
+            const authHeader = req.headers.authorization;
+            if (!authHeader || !authHeader.startsWith("Bearer ")) {
+                return res.status(401).json({ message: "Нет токена" });
+            }
+
+            const token = authHeader.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // @ts-ignore
+            req.user = decoded; // id + role
+
+            const user = await User.findById(req.user.id);
+            if (!user) return res.status(404).json({ message: "Пользователь не найден" });
+
+            next();
+        } catch (err) {
+            console.error("AUTH ERROR:", err);
+            return res.status(401).json({ message: "Неверный токен" });
         }
-
-        const token = authHeader.split(" ")[1];
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = {
-            id: decoded.id,   // 🔴 ВАЖНО
-        };
-        req.admin = decoded;
-
-        next();
-    } catch (e) {
-        return res.status(401).json({ message: "Не авторизован" });
-    }
+    };
 };
